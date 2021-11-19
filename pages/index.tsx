8089860223ae428c9b1fe2f07dea0313ee01054c
MikevPeeren <mikevpeeren@hotmail.com>
2021-11-19 14:00:57 +0100

    :sparkles: added image

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 
+import EQUILIBRIUM from "../public/image-equilibrium.jpg";
+
 const Home: NextPage = () => {
   return (
     <div>
@@ -11,7 +13,19 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="bg-dark-blue-one min-h-screen"></main>
+      <main className="bg-dark-blue-one min-h-screen flex flex-col justify-center items-center">
+        <div className="bg-dark-blue-two w-2/12 flex justify-center items-center rounded-2xl">
+          <div className="w-full h-2/4 my-8 mx-4">
+            <Image
+              src={EQUILIBRIUM}
+              className="rounded-2xl"
+              width={200}
+              height={200}
+              layout="responsive"
+            />
+          </div>
+        </div>
+      </main>
 
       <footer></footer>
     </div>
