35569420126b42ee3c6207d20c8719137ece144e
MikevPeeren <mikevpeeren@hotmail.com>
2021-11-19 13:40:25 +0100

    :lipstick: Adding colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "soft-blue": "hsl(215, 51%, 70%)",
+        cyan: "hsl(178, 100%, 50%)",
+        "dark-blue-one": "hsl(217, 54%, 11%)",
+        "dark-blue-two": "hsl(216, 50%, 16%)",
+        "dark-blue-three": "hsl(215, 32%, 27%)",
+      },
+    },
   },
   variants: {
     extend: {},
