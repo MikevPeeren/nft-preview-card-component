990313687a43aeca227dd14d3881bdb9b112700a
MikevPeeren <mikevpeeren@hotmail.com>
2021-11-19 13:32:34 +0100

    :tada: Starting new project

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Four Card Feature Section</title>
+        <meta name="description" content="Four Card Feature Section" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+
+      <footer></footer>
+    </div>
+  );
+};
+
+export default Home;
