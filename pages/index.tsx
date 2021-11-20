9a0c40f1e8c4acaaf625d0c2b67aaf6c9404413a
MikevPeeren <mikevpeeren@hotmail.com>
2021-11-20 05:56:47 +0100

    :dizzy: added hover states

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import EQUILIBRIUM from "../public/image-equilibrium.jpg";
 import ETHEREUM_ICON from "../public/icon-ethereum.svg";
 import CLOCK_ICON from "../public/icon-clock.svg";
+import VIEW_ICON from "../public/icon-view.svg";
 import AVATAR from "../public/image-avatar.png";
 
 const Home: NextPage = () => {
@@ -18,17 +19,34 @@ const Home: NextPage = () => {
 
       <main className="bg-dark-blue-one min-h-screen flex flex-col justify-center items-center">
         <div className="bg-dark-blue-two w-10/12 md:w-5/12 lg:w-4/12 xl:w-3/12 2xl:w-3/12 flex flex-col justify-center items-start rounded-2xl px-6">
-          <div className="w-full my-6">
+          <div className="w-full h-auto my-6 overflow-hidden relative">
             <Image
               src={EQUILIBRIUM}
+              alt="Image of the NFT Equilibrium"
               className="rounded-2xl"
               width={200}
               height={200}
+              priority
               layout="responsive"
             />
+            <div className="nftImageOverlay bg-cyan bg-opacity-50 opacity-0 hover:opacity-100 hover:cursor-pointer rounded-xl absolute w-full h-full top-1/2 left-1/2 flex justify-center items-center transition duration-700 ease-in-out">
+              <a href="https://mikevpeeren.nl">
+                <Image
+                  src={VIEW_ICON}
+                  alt=""
+                  className="rounded-2xl"
+                  width={48}
+                  height={48}
+                  priority
+                  layout="fixed"
+                />
+              </a>
+            </div>
           </div>
           <div>
-            <h1>Equilibrium #3429</h1>
+            <h1 className="hover:text-cyan cursor-pointer">
+              Equilibrium #3429
+            </h1>
             <h2 className="mt-2">
               Our Equilibrium collection promotes balance and calm.
             </h2>
@@ -38,6 +56,7 @@ const Home: NextPage = () => {
               <div className="flex items-center">
                 <Image
                   src={ETHEREUM_ICON}
+                  alt=""
                   width={11}
                   height={18}
                   layout="fixed"
@@ -47,7 +66,13 @@ const Home: NextPage = () => {
             </div>
             <div className="flex flex-row">
               <div className="flex items-center">
-                <Image src={CLOCK_ICON} width={17} height={17} layout="fixed" />
+                <Image
+                  src={CLOCK_ICON}
+                  alt=""
+                  width={17}
+                  height={17}
+                  layout="fixed"
+                />
               </div>
               <p className="px-2 text-soft-blue">3 days left</p>
             </div>
@@ -59,6 +84,7 @@ const Home: NextPage = () => {
             <div className="flex flex-row items-center">
               <Image
                 src={AVATAR}
+                alt="Sellor of the NFT, in this case Jules Wyvern"
                 className="imageBorder"
                 width={30}
                 height={30}
@@ -67,9 +93,12 @@ const Home: NextPage = () => {
             </div>
             <p className="mx-2 text-white font-normal">
               <span className="text-soft-blue">Creation of </span>
-              <span className="hover:text-cyan cursor-pointer">
+              <a
+                href="https://mikevpeeren.nl"
+                className="hover:text-cyan cursor-pointer"
+              >
                 Jules Wyvern
-              </span>
+              </a>
             </p>
           </div>
         </div>
